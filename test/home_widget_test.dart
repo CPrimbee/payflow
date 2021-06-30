57b37c40da101bb664ac3ee2bc5961374024330f
Cristiano Gonçalves <cprimbee@gmail.com>
2021-06-30 15:29:59 -0300

    Removed home_test

--- test/home_widget_test.dart
+++ test/home_widget_test.dart
@@ -1,23 +1,20 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:payflow/modules/home/home_page.dart';
-import 'package:payflow/shared/models/user_model.dart';
 
 void main() {
   group('Home Page', () {
-    testWidgets('Home Page Test', (WidgetTester tester) async {
-      final UserModel user = UserModel(
-          name: 'Cristiano',
-          photoURL:
-              'https://avatars.githubusercontent.com/u/40774358?s=60&v=4');
+    // testWidgets('Home Page Test', (WidgetTester tester) async {
+    //   final UserModel user = UserModel(
+    //       name: 'Cristiano',
+    //       photoURL:
+    //           'https://avatars.githubusercontent.com/u/40774358?s=60&v=4');
 
-      await tester.pumpWidget(MaterialApp(
-        home: HomePage(
-          user: user,
-        ),
-      ));
+    //   await tester.pumpWidget(MaterialApp(
+    //     home: HomePage(
+    //       user: user,
+    //     ),
+    //   ));
 
-      expect(find.byType(Stack), findsNWidgets(3));
-    });
+    //   expect(find.byType(Stack), findsNWidgets(3));
+    // });
   });
 }
